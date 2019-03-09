3d23255f985d8c810ecb7df2097f33fafa3c5ee5
AlexisChemin <alexis.chemin@gmail.com>
2019-03-09 18:48:50 +0100

    Initial commit

---
+++ src/test/kotlin/TestSource.kt
@@ -0,0 +1,11 @@
+import kotlin.test.assertEquals
+import org.junit.Test as test
+
+class TestSource {
+    @test fun f() {
+        val example  = Greeter("Hi")
+        val greeting = example.greeting
+
+        assertEquals("Hi", greeting)
+    }
+}
